528541f28ac1e694f93ff9fb29db707a0a848b27
shamsullah-shams <shamsullah07073@gmail.com>
2024-08-31 16:51:39 +0430

    multer middleware added

---
+++ src/middlewares/multer.js
@@ -0,0 +1,37 @@
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, './src/storage/images');
+  },
+  filename(req, file, cb) {
+    cb(null, Date.now() + Math.floor(Math.random() * 1000000) + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/JPG' ||
+    file.mimetype === 'image/PNG' ||
+    file.mimetype === 'image/jpeg' ||
+    file.mimetype === 'image/JPEG' ||
+    file.mimetype === 'application/pdf'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: {
+    // Max Size of Image 250mb
+    fileSize: 250 * 1024 * 1024,
+  },
+});
+
+module.exports = upload;
